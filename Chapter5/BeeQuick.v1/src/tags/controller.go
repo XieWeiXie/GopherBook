2df741a5889ebd2bccf3141c012decd4ac10badd
XieWei <wuxiaoshen@shu.edu.cn>
2019-06-17 19:28:49 +0800

    feat: tags

---
+++ Chapter5/BeeQuick.v1/src/tags/controller.go
@@ -0,0 +1,11 @@
+package tags
+
+import "github.com/kataras/iris"
+
+func postTagHandle(ctx iris.Context) {}
+
+func patchTagHandle(ctx iris.Context) {}
+
+func getTagHandle(ctx iris.Context) {}
+
+func getTagsHandle(ctx iris.Context) {}
---
+++ Chapter5/BeeQuick.v1/src/tags/router.go
@@ -0,0 +1,20 @@
+package tags
+
+import "github.com/kataras/iris"
+
+type ControllerTags struct {
+}
+
+var Default = ControllerTags{}
+
+func (controller ControllerTags) Register(application *iris.Application, path string) {
+
+	tags := application.Party(path, func(context iris.Context) {
+		context.Next()
+	})
+
+	tags.Get("/tags", getTagsHandle)
+	tags.Get("/tag/{tag_id:int}", getTagHandle)
+	tags.Post("/tag", postTagHandle)
+	tags.Patch("/tag/{tag_id:int}", patchTagHandle)
+}
