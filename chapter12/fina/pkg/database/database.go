66245fbc67c51212ee6976f71610c52f868742c7
XieWei <wuxiaoshen@shu.edu.cn>
2019-08-01 00:19:17 +0800

    FEAT

---
+++ chapter12/fina/pkg/database/database.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"fmt"
+	"github.com/go-xorm/xorm"
+	"github.com/xormplus/core"
+)
+
+var MySQL *xorm.Engine
+
+var (
+	dbMySQL    = "fifa"
+	dbUser     = "root"
+	dbPassword = "admin123"
+)
+
+func MySQLInit() *xorm.Engine {
+	engine, err := xorm.NewEngine("mysql", fmt.Sprintf("%s:%s@/%s?charset=utf8",dbUser, dbPassword, dbMySQL)
+	if err!=nil{
+		panic(fmt.Sprintf("CONNECT ENGINE BY XORM FAIL %s",err.Error()))
+	}
+	engine.SetTableMapper(core.SameMapper{})
+	engine.SetMapper(core.GonicMapper{})
+	MySQL = engine
+	MySQL.ShowSQL(true)
+	return MySQL
+
+
+}
+
+var POSTGRES *xorm.Engine
+var (
+	dbPGSQL      = "fifa"
+	dbPGUser     = "postgres"
+	dbPGPassword = "admin123"
+)
+
+func PostgreSQLInit() *xorm.Engine {
+	return POSTGRES
+}
